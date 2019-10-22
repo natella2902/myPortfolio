4a712e8b0d6eb4b4c6199157f29fadb58a5bfd9d
natella <n.jelonkina@mail.ru>
2019-10-22 03:15:26 +0300

    w2 swiper slider

--- src/scripts/works.js
+++ src/scripts/works.js
@@ -17,17 +17,23 @@ const thumbs = {
 const display = {
   template: "#slider-display",
   components: { thumbs, btns },
-  props: ["works", "currentWork"]
+  props: ["works", "currentWork", "currentIndex"]
 }
 
 const tags = {
-  template: "#slider-tags"
+  template: "#slider-tags",
+  props: ["tags"]
 }
 
 const info = {
   template: "#slider-info",
   components: { tags },
-  props: ["currentWork"]
+  props: ["currentWork"],
+  computed: {
+    tagsArray() {
+      return this.currentWork.skills.split(', ');
+    }
+  }
 }
 
 new Vue({
@@ -43,7 +49,19 @@ new Vue({
       return this.works[this.currentIndex];
     }
   },
+  watch: {
+    currentIndex(value) {
+      this.makeInfiniteLoop(value) ;
+    }
+     
+  },
   methods: {
+    makeInfiniteLoop(value) {
+      const worksAmount = this.works.length - 1;
+      if (value < 0) this.currentIndex = worksAmount ;
+      if (value > worksAmount) this.currentIndex = 0
+    },
+
     makeArrWithRequiredImages(data) {
       return data.map(item => {
         const requiredPic = require(`../images/content/${item.photo}`);
